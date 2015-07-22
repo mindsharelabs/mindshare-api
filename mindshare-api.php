87e9e9467fb06018278fc09146113ee5b2faa180
Damian Taggart <damian@mindsharestudios.com>
2015-07-22 16:01:54 -0600

    bug fix for add_menu_page on menus

--- mindshare-api.php
+++ mindshare-api.php
@@ -4,7 +4,7 @@ Plugin Name: Mindshare Theme API
 Plugin URI: https://mindsharelabs.com/downloads/mindshare-theme-api/
 Description: Provides a library of additional template tags, 3rd-party libraries, and functions for WordPress themes and additional features for WordPress CMS websites.
 Author: Mindshare Studios, Inc
-Version: 0.8.9
+Version: 0.9
 Author URI: https://mind.sh/are/
 Network: false
 */
@@ -106,7 +106,7 @@ if(!class_exists("Mindshare_API")) :
 		 *
 		 * @var string
 		 */
-		private $class_version = '0.8.9';
+		private $class_version = '0.9';
 
 		/**
 		 * Used for automatic updates
