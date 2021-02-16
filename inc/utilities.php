03a67c5d5279ec7de3cb2aeeaa2905af0cb23355
James W Johnson <me@thejameswilliam.com>
2021-02-16 11:33:28 -0700

    Update utilities.php
    Fixed a conflict with some mindshare themes.

--- inc/utilities.php
+++ inc/utilities.php
@@ -5,7 +5,7 @@ if (function_exists('add_image_size')) {
   add_image_size( 'loop-thumbnail', 350, 150, array('center', 'center'));
 }
 
-if(function_exists('mapi_var_dump')) {
+if(!function_exists('mapi_var_dump')) {
   function mapi_var_dump($var) {
     if (current_user_can('administrator') && isset($var)) {
         echo '<pre>';
