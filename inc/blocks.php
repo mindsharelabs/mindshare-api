33e5a2fe7b836aeca3b76a27bf88f41eb87f3402
James W. Johnson <me@thejameswilliam.com>
2021-04-12 11:00:46 -0600

    added fblock settings for gutenberg blocks.

--- inc/blocks.php
+++ inc/blocks.php
@@ -247,6 +247,92 @@ add_action('acf/init', function () {
 
 if( function_exists('acf_add_local_field_group') ):
 
+
+	//Block: Media and Image
+	acf_add_local_field_group(array(
+		'key' => 'group_60745cc009296',
+		'title' => 'Block: Media and Image',
+		'fields' => array(
+			array(
+				'key' => 'field_60745cc61d382',
+				'label' => 'Image and Content',
+				'name' => 'image_and_content',
+				'type' => 'group',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'layout' => 'block',
+				'sub_fields' => array(
+					array(
+						'key' => 'field_60745d0d1d384',
+						'label' => 'Image',
+						'name' => 'image',
+						'type' => 'image',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'return_format' => 'array',
+						'preview_size' => 'medium',
+						'library' => 'all',
+						'min_width' => '',
+						'min_height' => '',
+						'min_size' => '',
+						'max_width' => '',
+						'max_height' => '',
+						'max_size' => '',
+						'mime_types' => '',
+					),
+					array(
+						'key' => 'field_60745d1d1d385',
+						'label' => 'Content',
+						'name' => 'content',
+						'type' => 'wysiwyg',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'tabs' => 'all',
+						'toolbar' => 'full',
+						'media_upload' => 1,
+						'delay' => 0,
+					),
+				),
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'block',
+					'operator' => '==',
+					'value' => 'acf/image-and-content',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+	));
+
 	//ACF Block Fields: Map w/ Marker *
 	acf_add_local_field_group(array(
 		'key' => 'group_5fad8bfb7b985',
