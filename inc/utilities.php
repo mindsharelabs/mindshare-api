9d633a4879bc89ea8b684bf529b766c11e3e7bb0
James W. Johnson <me@thejameswilliam.com>
2021-04-12 13:39:48 -0600

    fixed some bugs in map marker display.

--- inc/utilities.php
+++ inc/utilities.php
@@ -8,6 +8,31 @@ if (function_exists('add_image_size')) {
   add_image_size( 'horizontal-media-image', 500, 400, array('center', 'center'));
 }
 
+
+
+add_filter( 'render_block', 'mapi_block_wrapper', 10, 2 );
+function mapi_block_wrapper( $block_content, $block ) {
+  $noWrapper = array(
+    'acf/map-w-marker',
+    'core/cover',
+    'core/button',
+    'acf/container',
+    'acf/image-and-content'
+  );
+  if(!in_array($block['blockName'], $noWrapper) && $block['blockName']) :
+    $content = '<div class="container">';
+      $content .= '<div class="row">';
+        $content .= '<div class="col-12">';
+          $content .= $block_content;
+        $content .= '</div>';
+      $content .= '</div>';
+    $content .= '</div>';
+  else :
+    return $block_content;
+  endif;
+  return $content;
+}
+
 if(!function_exists('mapi_var_dump')) {
   function mapi_var_dump($var) {
     if (current_user_can('administrator') && isset($var)) {
