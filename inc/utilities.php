884ee250b0850790129b6217c9e314443f917801
James W. Johnson <me@thejameswilliam.com>
2021-05-20 08:37:56 -0600

    remove admin bar for non-admins.

--- inc/utilities.php
+++ inc/utilities.php
@@ -36,6 +36,13 @@ if(!function_exists('mapi_write_log')) {
 	}
 }
 
+add_action('after_setup_theme', function () {
+  if (!current_user_can('administrator') && !is_admin()) {
+    show_admin_bar(false);
+  }
+});
+
+
 
 add_filter( 'mapi_block_wrappers', function($noWrapper) {
   $noWrapper[] = 'acf/image-slider';
