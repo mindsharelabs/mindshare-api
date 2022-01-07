bfe2cbdde58184eb309f17bba8c21062dad0199a
James W. Johnson <me@thejameswilliam.com>
2022-01-07 09:43:17 -0700

    updated the style and layout of custom popup.

--- inc/popups/popup.php
+++ inc/popups/popup.php
@@ -12,16 +12,18 @@ add_action('wp_footer', function() {
       echo '<div class="modal fade" id="mindModal" ' . ($options['behavior'] == 'static' ? 'data-bs-backdrop="static"' : '') . ' data-bs-keyboard="false" tabindex="-1" aria-labelledby="mindModalLabel" aria-hidden="true">';
         echo '<div class="modal-dialog ' . ($options['position'] == 'centered' ? 'modal-dialog-centered' : '') . ' modal-' . $options['size'] . '">';
           echo '<div class="modal-content">';
-            echo '<div class="modal-header">';
+            echo '<div class="close-btn d-flex justify-content-end">';
+              echo '<button type="button" class="btn btn-danger" data-bs-dismiss="modal" aria-label="Close"><i class="fas fa-times"></i></button>';
+            echo '</div>';
+            echo '<div class="modal-header text-center">';
               echo '<h3 class="modal-title" id="mindModalLabel">' . $header . '</h3>';
-              echo '<button type="button" class="btn btn-default" data-bs-dismiss="modal" aria-label="Close"><i class="fas fa-times"></i></button>';
             echo '</div>';
             echo '<div class="modal-body">' . $content . '</div>';
-            if($link) :
-              echo '<div class="modal-footer d-flex text-center justify-content-center">';
-                echo '<a href="' . $link['url'] . '" target="' . $link['target'] . '" title="' . $link['title'] . '" class="btn btn-lg btn-secondary">' . $link['title'] . '</a>';
-              echo '</div>';
-            endif;
+            echo '<div class="modal-footer d-flex text-center justify-content-center">';
+              if($link) :
+                echo '<a href="' . $link['url'] . '" target="' . $link['target'] . '" title="' . $link['title'] . '" class="btn btn-primary me-3">' . $link['title'] . '</a>';
+              endif;
+            echo '</div>';
           echo '</div>';
         echo '</div>';
       echo '</div>';
