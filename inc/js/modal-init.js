aea3874df569d3dca3ba3fda51d2df9f3521c706
James W. Johnson <me@thejameswilliam.com>
2022-01-07 10:53:18 -0700

    added cookies to modal so it only shows one time to useers.

--- inc/js/modal-init.js
+++ inc/js/modal-init.js
@@ -13,13 +13,15 @@
         keyboard: false
       });
       if(mindModalSettings.show) {
-        mindModal.show();
+				var modalCookie = Cookies.get('mind-notice-modal');
+				console.log(modalCookie);
+				var modalid = $('#mindNoticeModal').data('modalid');
+				if(modalCookie != modalid) {
+					mindModal.show();
+					Cookies.set('mind-notice-modal', modalid, { expires: 1 })
+				}
+
       }
-
-
-
-
-
 
 
 
