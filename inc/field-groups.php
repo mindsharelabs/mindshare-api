e5ba166c936e83100c157c4a2fe1ad777fbd0e00
James W Johnson <me@thejameswilliam.com>
2023-05-16 09:39:23 -0600

    removed scripts that supported custom popups.

--- inc/field-groups.php
+++ inc/field-groups.php
@@ -2,13 +2,13 @@
 
 if( function_exists('acf_add_options_page') ) {
 
-  acf_add_options_page(array(
-    'page_title' 	=> 'Custom Popup',
-    'menu_title'	=> 'Custom Popup',
-    'menu_slug' 	=> 'website-popup',
-    'capability'	=> 'edit_posts',
-    'redirect'		=> false
-  ));
+  // acf_add_options_page(array(
+  //   'page_title' 	=> 'Custom Popup',
+  //   'menu_title'	=> 'Custom Popup',
+  //   'menu_slug' 	=> 'website-popup',
+  //   'capability'	=> 'edit_posts',
+  //   'redirect'		=> false
+  // ));
 
   acf_add_options_page(array(
     'page_title' 	=> 'API Settings',
