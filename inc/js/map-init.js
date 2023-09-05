589ce6b41dcdee9028ee413a3d01dff2015d8d13
James W Johnson <me@thejameswilliam.com>
2023-09-05 15:09:48 -0600

    marker clustering for map locations.

--- inc/js/map-init.js
+++ inc/js/map-init.js
@@ -1,127 +1,69 @@
-(function( $ ) {
 
 
-console.log(map_param_data);
 
-/**
- * initMap
- *
- * Renders a Google Map onto the selected jQuery element
- *
- * @date    22/10/19
- * @since   5.8.6
- *
- * @param   jQuery $el The jQuery element.
- * @return  object The map instance.
- */
-function initMap( $el ) {
 
-    // Find marker elements within map.
-    var $markers = $el.find('.marker');
-
-    // Create gerenic map.
-    var mapArgs = {
-        zoom        : $el.data('zoom') || 16,
-        mapTypeId   : google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map( $el[0], mapArgs );
-
-    // Add markers.
-    map.markers = [];
-    $markers.each(function(){
-        initMarker( $(this), map );
+async function initMap($el) {
+    
+    // Request needed libraries.
+    const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+      "marker",
+    );
+ 
+    const map = new google.maps.Map($el[0], {
+      zoom: $el.data('zoom') || 12,
+      center: { lat: 35.6870, lng: -105.9378 },
+      mapId: "DEMO_MAP_ID",
     });
-
-    // Center map based on markers.
-    centerMap( map );
-
-    // Return map instance.
-    return map;
-}
-
-/**
- * initMarker
- *
- * Creates a marker for the given jQuery element and map.
- *
- * @date    22/10/19
- * @since   5.8.6
- *
- * @param   jQuery $el The jQuery element.
- * @param   object The map instance.
- * @return  object The marker instance.
- */
-function initMarker( $marker, map ) {
-
-    // Get position from marker.
-    var lat = $marker.data('lat');
-    var lng = $marker.data('lng');
-    var latLng = {
-        lat: parseFloat( lat ),
-        lng: parseFloat( lng )
-    };
-
-    // Create marker instance.
-    var marker = new google.maps.Marker({
-        position : latLng,
-        map: map
+    
+    const infoWindow = new google.maps.InfoWindow({
+        content: "",
+        disableAutoPan: true,
     });
+    
+    var latlngbounds = new google.maps.LatLngBounds();
 
-    // Append to reference for later use.
-    map.markers.push( marker );
-
-    // If marker contains HTML, add it to an infoWindow.
-    if( $marker.html() ){
-
-        // Create info window.
-        var infowindow = new google.maps.InfoWindow({
-            content: $marker.html()
+    const markers = locations.map((position, i) => {
+        const label = (i + 1).toString();
+ 
+        const pinGlyph = new google.maps.marker.PinElement({
+            glyph: label,
+            glyphColor: "white",
+        });
+        const marker = new google.maps.marker.AdvancedMarkerElement({
+            position,
+            content: pinGlyph.element,
         });
 
-        // Show info window when marker is clicked.
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open( map, marker );
+        latlngbounds.extend(marker.position);
+        // markers can only be keyboard focusable when they have click listeners
+        // open info window when marker is clicked
+        marker.addListener("click", () => {
+          infoWindow.setContent(position.lat + ", " + position.lng);
+          infoWindow.open(map, marker);
         });
-    }
-}
-
-/**
- * centerMap
- *
- * Centers the map showing all markers in view.
- *
- * @date    22/10/19
- * @since   5.8.6
- *
- * @param   object The map instance.
- * @return  void
- */
-function centerMap( map ) {
-
-    // Create map boundaries from all map markers.
+        return marker;
+      });
+      
     var bounds = new google.maps.LatLngBounds();
-    map.markers.forEach(function( marker ){
-        bounds.extend({
-            lat: marker.position.lat(),
-            lng: marker.position.lng()
-        });
-    });
+    map.setCenter(latlngbounds.getCenter());
+    map.fitBounds(latlngbounds);
 
-    // Case: Single marker.
-    if( map.markers.length == 1 ){
-        map.setCenter( bounds.getCenter() );
-
-    // Case: Multiple markers.
-    } else{
-        map.fitBounds( bounds );
-    }
+    // Add a marker clusterer to manage the markers.
+    new markerClusterer.MarkerClusterer({ markers, map });
 }
 
-// Render maps on page load.
-$(document).ready(function(){
-    $('.acf-map').each(function(){
-        var map = initMap( $(this) );
-    });
-});
+  
 
+  
+(function($) {
+  // Render maps on page load.
+    $(document).ready(function(){
+        
+        $('.acf-map').each(function(){
+            initMap( $(this) );   
+            
+        });
+    });
+  
 })(jQuery);
